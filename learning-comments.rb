e61583677d892d95329a26cf60ad24df58290461
Alison Quaglia <alison.v.quaglia@gmail.com>
2020-07-29 22:28:42 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,53 @@
+# Perform a constant expression evaluation
+#3
+
+# Assign constant 3 to bare-word variable triangle_sides
+#`triangle_sides`
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+#puts "Joy to the world"
+#puts "All the boys and girls"
+#puts "Joy to the fishes in the deep blue sea"
+#puts "Joy to you and me"
+
+#name = "Byron"
+# name = "Luca"
+
+#puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
+
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+ # puts "code inside"
+#end
+#puts "Code after if...end"
+
+# chance_of_rain = -23  
+#puts "Let's go outside!"
+# if chance_of_rain <= 0.25  
+#   puts "Pack a sun shelter!"
+# elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#   puts "Pack an umbrella!"
+# else 
+#   puts "Stay home and read Hegel."
+# end
+#puts "Oh, and always wear sunscreen!"
+
+# 
+
+# count = 0  
+# n = 3
+
+# loop do   
+#   break if count >= n 
+#   puts "I ran."
+#   count += 1 
+# end 
+
+
+def a_method(a, b)
+  puts "hi"
+  a + b 
+end
+
+puts a_method(1, 2)
